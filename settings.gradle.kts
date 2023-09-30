f04e1a2590629510209df2d3e0764c7fc4dee263
Marcin Piekielny <marcinpiekielny@MacBook-Pro-Marcin.local>
2023-09-30 16:15:46 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "gymster-multiplatform"
+include(":androidApp")
+include(":shared")
