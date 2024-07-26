85e8e66f49c3d85dd8eb48ed90fc9ebc0b61fb66
Marcin Piekielny <marcinpk.mp@gmail.com>
2024-07-27 00:38:34 +0200

    Format code

--- shared/src/commonMain/kotlin/com/maruchin/multiplatform/gymster/Greeting.kt
+++ shared/src/commonMain/kotlin/com/maruchin/multiplatform/gymster/Greeting.kt
@@ -3,7 +3,5 @@ package com.maruchin.multiplatform.gymster
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
+    fun greet(): String = "Hello, ${platform.name}!"
 }
