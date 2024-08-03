cadaa8f9cd78a778f31220df069246decf88d8a6
Marcin Piekielny <marcinpk.mp@gmail.com>
2024-08-03 16:59:59 +0200

    Update MainNavigationBar

--- androidApp/src/main/java/com/maruchin/gymster/android/MainNavigationBar.kt
+++ androidApp/src/main/java/com/maruchin/gymster/android/MainNavigationBar.kt
@@ -5,9 +5,14 @@ import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.FitnessCenter
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.FitnessCenter
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -26,38 +31,67 @@ internal fun MainNavigationBar(navController: NavController) {
     val backStackEntry by navController.currentBackStackEntryAsState()
     val currentRoutes = backStackEntry?.destination?.hierarchy?.map { it.route }.orEmpty()
     NavigationBar {
+        val isHomeSelected = currentRoutes.contains(HomeGraph::class.qualifiedName)
+        val isTrainingsSelected = currentRoutes.contains(TrainingsGraph::class.qualifiedName)
+        val isPlansSelected = currentRoutes.contains(PlansGraph::class.qualifiedName)
+        val isProfileSelected = false
+
         NavigationBarItem(
-            selected = currentRoutes.contains(HomeGraph::class.qualifiedName),
+            selected = isHomeSelected,
             onClick = {
                 navController.navigateToItem(HomeGraph)
             },
             icon = {
-                Icon(imageVector = Icons.Default.Home, contentDescription = null)
+                Icon(
+                    imageVector = if (isHomeSelected) Icons.Default.Home else Icons.Outlined.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "Home")
             }
         )
         NavigationBarItem(
-            selected = currentRoutes.contains(TrainingsGraph::class.qualifiedName),
+            selected = isTrainingsSelected,
             onClick = {
                 navController.navigateToItem(TrainingsGraph)
             },
             icon = {
-                Icon(imageVector = Icons.Default.FitnessCenter, contentDescription = null)
+                Icon(
+                    imageVector = if (isTrainingsSelected) Icons.Default.FitnessCenter else Icons.Outlined.FitnessCenter,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "Trainings")
             }
         )
         NavigationBarItem(
-            selected = currentRoutes.contains(PlansGraph::class.qualifiedName),
+            selected = isPlansSelected,
             onClick = {
                 navController.navigateToItem(PlansGraph)
             },
             icon = {
-                Icon(imageVector = Icons.Default.CalendarMonth, contentDescription = null)
+                Icon(
+                    imageVector = if (isPlansSelected) Icons.Default.CalendarMonth else Icons.Outlined.CalendarMonth,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "Plans")
             }
         )
         NavigationBarItem(
-            selected = false,
+            selected = isProfileSelected,
             onClick = { /*TODO*/ },
             icon = {
-                Icon(imageVector = Icons.Default.Person, contentDescription = null)
+                Icon(
+                    imageVector = if (isProfileSelected) Icons.Default.Person else Icons.Outlined.Person,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "Profile")
             }
         )
     }
