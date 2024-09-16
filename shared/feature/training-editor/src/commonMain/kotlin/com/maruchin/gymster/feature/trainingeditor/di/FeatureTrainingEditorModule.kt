72bf885a443908538a18eaf512fa842817b298bc
Marcin Piekielny <marcinpk.mp@gmail.com>
2024-09-16 22:15:12 +0200

    Remove unused modules

--- shared/feature/training-editor/src/commonMain/kotlin/com/maruchin/gymster/feature/trainingeditor/di/FeatureTrainingEditorModule.kt
+++
@@ -1,9 +0,0 @@
-package com.maruchin.gymster.feature.trainingeditor.di
-
-import com.maruchin.gymster.feature.trainingeditor.editor.TrainingEditorViewModel
-import org.koin.dsl.module
-
-val featureTrainingEditorModule = module {
-
-    factory { TrainingEditorViewModel(it[0], it[1], get()) }
-}
